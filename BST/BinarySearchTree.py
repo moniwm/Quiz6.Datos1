fd2afc12aa35e667c6e2707200a7e0f9a6bd4e5d
Mónica Waterhouse <mwaterhouse@estudiantec.cr>
2020-06-27 00:17:00 -0600

    Create remove method and findMin method

--- BST/BinarySearchTree.py
+++ BST/BinarySearchTree.py
@@ -23,7 +23,42 @@ class BinarySearchTree:
             else:
                 self._insert(value, node.right)
         else:
-            print ("The value is already in the tree")
+            print("The value is already in the tree")
+
+    def findMin(self):
+        if self.root is None:
+            return None
+        else:
+            return self._findMin(self.root)
+
+    def _findMin(self, node):
+        if node is None:
+            return None
+        elif (node.left is None):
+            return node
+        else:
+            return self._findMin(node.left)
+
+    def remove(self, value):
+        self.root = self._remove(value, self.root)
+
+    def _remove(self, value, node):
+        if node is None:
+            return None
+        if value < node.value:
+            node.left = self._remove(value, node.left)
+        elif value > node.value:
+            node.right = self._remove(value, node.right)
+        elif node.left is not None and node.right is not None:
+            node.value = self._findMin(node.right)
+            node.right = self._remove(node.value, node.right);
+        else:
+            if node.left is not None:
+                node = node.left
+            else:
+                node = node.right
+        return node
+
 
     def printTree(self):
         if self.root is not None:
@@ -34,4 +69,7 @@ class BinarySearchTree:
             self._printTree(node.left)
             print(str(node.value))
             self._printTree(node.right)
+
+
+
 
--- BST/Main.py
+++ BST/Main.py
@@ -1,17 +1,30 @@
-from random import randint
-
 from BST.BinarySearchTree import BinarySearchTree
 
 
-def fillTree(bstTree, num=100):
-    for i in range(num):
-        element = randint(0, 1000)
-        bstTree.insert(element)
-    return bstTree
-
-
 bstTree = BinarySearchTree()
-bstTree = fillTree(bstTree)
+
+bstTree.insert(9)
+bstTree.insert(8)
+bstTree.insert(17)
+bstTree.insert(14)
+bstTree.insert(7)
+bstTree.insert(10)
+bstTree.insert(19)
+bstTree.insert(29)
+bstTree.insert(45)
+bstTree.insert(87)
+
 bstTree.printTree()
+
+bstTree.remove(10)
+
+bstTree.printTree()
+
+bstTree.remove(7)
+bstTree.remove(19)
+
+bstTree.printTree()
+
+
 
 
